8dfe7065d9c25d4f3cbb4894905c81696b13dbae
Ameya Damle <89841648+daameya@users.noreply.github.com>
2024-07-18 10:27:35 +0530

    airflow pipeline created using docker compose

---
+++ etls/reddit_etl.py
@@ -0,0 +1,26 @@
+import sys
+
+import numpy as np
+import pandas as pd
+import praw
+from praw import Reddit
+
+def connect_reddit(client_id, client_secret, user_agent) -> Reddit:
+    try:
+        reddit = praw.Reddit(client_id = client_id, 
+                             client_secret = client_secret, 
+                             user_agent = user_agent)
+        print("Connected to reddit!")
+        return reddit
+    except Exception as e:
+        print(e)
+        sys.exit(1)
+
+def extract_posts(reddit_instance: Reddit, subreddit: str, time_filter: str, limit=None):
+    subreddit = reddit_instance.subreddit(subreddit)
+    posts = subreddit.top(time_filter=time_filter, limit=limit)
+
+    post_lists = []
+
+    print(posts)
+    #for post in posts:
